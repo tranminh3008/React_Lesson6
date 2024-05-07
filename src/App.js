0efabc171bc4ade33fa6e2db227eb2e836a997c7
tranminh3008 <tranhuuminh3008@gmail.com>
2024-05-07 10:06:02 +0700

    weather

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import API1 from "./components/API1";
 import Weather from "./components/weather/Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="main">
-      {/* <API1 /> */}
       <Weather />
     </div>
   );
